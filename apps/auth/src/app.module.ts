8e6e5f57350816002ce62e40bd21f77646571e60
Edward <e.arechwa@gmail.com>
2025-09-17 12:45:39 +0200

    feat: add graphql and nest lib

--- apps/auth/src/app.module.ts
+++ apps/auth/src/app.module.ts
@@ -1,7 +1,16 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { PrismaModule } from './modules/prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+      playground: true,
+    }),
+  ],
 })
 export class AppModule {}
