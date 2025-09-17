a3070e21a1b16bb6e2d64e8e9adcae06b6633922
Edward <e.arechwa@gmail.com>
2025-09-17 12:35:19 +0200

    feat: add prisma module and service

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,20 +1,17 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app/app.module';
+import { AppModule } from './app.module';
+
+const APP_PREFIX = 'api';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
+  app.setGlobalPrefix(APP_PREFIX);
   const port = process.env.PORT || 3000;
   await app.listen(port);
+
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+    `🚀 Application is running on: http://localhost:${port}/${APP_PREFIX}`
   );
 }
 
