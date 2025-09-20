4277f72eb61c514365b7d3e1dd7c5ea56af5eca6
Edward <e.arechwa@gmail.com>
2025-09-20 18:48:24 +0200

    feat: add build targets

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,7 +1,10 @@
+import { AUTH_PACKAGE_NAME } from '@nest-microservices-edu/nestjs';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { GrpcOptions, Transport } from '@nestjs/microservices';
 import cookieParser from 'cookie-parser';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -9,6 +12,16 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.use(cookieParser());
+
+  app.connectMicroservice<GrpcOptions>({
+    transport: Transport.GRPC,
+    options: {
+      package: AUTH_PACKAGE_NAME,
+      protoPath: join(__dirname, 'proto/auth.proto'),
+    },
+  });
+
+  app.startAllMicroservices();
 
   const port = app.get(ConfigService).get<number>('AUTH_PORT');
   await app.listen(port);
